b2060f163f075b8a27ae9a096f788d76f00c095f
yaott <yaotthah@foxmail.com>
2023-03-15 13:09:59 +0800

    update

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="user_id" label="职员编号" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="sex" label="性别" width="180"> </el-table-column>
+      <el-table-column prop="age" label="年龄" width="180"> </el-table-column>
+      <el-table-column prop="phone" label="手机号" width="180"> </el-table-column>
+      <el-table-column prop="email" label="邮箱" width="180"> </el-table-column>
+      <el-table-column prop="position" label="职位" width="180"> </el-table-column>
+      <el-table-column prop="marry" label="婚姻状况"> </el-table-column>
+      <el-table-column prop="education" label="教育程度"> </el-table-column>
+      <el-table-column prop="join_time" label="入职时间"> </el-table-column>
+      <el-table-column prop="department_name" label="所属部门"> </el-table-column>
+    </el-table>
   </div>
 </template>
+
+
 
 <script>
 import { mapGetters } from 'vuex'
@@ -13,6 +27,25 @@ export default {
     ...mapGetters([
       'name'
     ])
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          "user_id": 1,
+          "name": "张三",
+          "sex": "男",
+          "age": 18,
+          "phone": "12345678901",
+          "email": "zhangsan@example.com",
+          "position": "前端工程师",
+          "marry": "未婚",
+          "education": "本科",
+          "join_time": "2019-01-01",
+          "department_name": "技术部"
+        }
+      ]
+    }
   }
 }
 </script>
